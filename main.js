14956d4ce8b401e38d2aab1986cd839f1e604e07
santiago <santiago.reyes.1991@gmail.com>
2023-12-21 14:58:44 -0300

    version funcional

--- main.js
+++ main.js
@@ -1,12 +1,43 @@
-let edad1 = prompt("ingrese la edad del estudiante 1");
-let edad2 = prompt("ingrese la edad del estudiante 2");
-let edad3 = prompt("ingrese la edad del estudiante 3");
-let edad4 = prompt("ingrese la edad del estudiante 4");
-
 function calcularPromedio() {
-    let suma = edad1 + edad2 + edad3 + edad4;
-    let promedio = suma / 4;
+  let suma = 0;
+  let cuenta = 0;
+
+  while (true) {
+    let userInput = prompt("Ingresa la edad de cada estudiante o 'final' para terminar:");
+
+    // Corroborar final de ingreso de numeros
+    if (userInput.toLowerCase() === 'final') {
+      break;
+    }
+
+    // Convertir el ingreso a numero
+    let number = parseFloat(userInput);
+
+    // Corroborar que el ingreso sea un numero valido
+    if (!isNaN(number)) {
+      suma += number;
+      cuenta++;
+    } else {
+      alert("Ingresa un numero valido.");
+    }
+  }
+
+  // Corroborar que hay numeros para calcular el promedio
+  if (cuenta === 0) {
+    return null;
+  }
+
+  // Calcular promedio
+  let promedio = suma / cuenta;
+  return promedio;
 }
 
-alert("El promedio es " + promedio);
+let resultado = calcularPromedio();
 
+
+// Mostrar resultado
+if (resultado !== null) {
+  alert(`El promedio de edades es: ${resultado}`);
+} else {
+  alert("Por favor ingresa al menos un numero valido.");
+}
